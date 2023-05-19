8d868baf06628750b1734e6e2d5238cdfa8d8238
Elias Cyrino de Assis <eliascyrino@gmail.com>
2023-05-19 17:12:14 -0300

    [fix] returning name, email, phone

--- routes/pg.js
+++ routes/pg.js
@@ -9,7 +9,7 @@ module.exports = async function (fastify, opts) {
 
   fastify.post("/user", (request, reply) => {
     fastify.pg.query(
-      `INSERT INTO "user" (id, name, email, password) VALUES('${request.body.id}', '${request.body.name}', '${request.body.email}', '${request.body.password}') RETURNING *`,
+      `INSERT INTO "user" (id, name, email, password) VALUES('${request.body.id}', '${request.body.name}', '${request.body.email}', '${request.body.password}') RETURNING name, email, phone`,
       function onResult(err, result) {
         reply.send(err || result.rows);
       }
