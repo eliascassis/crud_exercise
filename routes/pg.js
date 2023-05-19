40de80edf0038e2a082ab9bca9c0ee33fb854e23
Elias Cyrino de Assis <eliascyrino@gmail.com>
2023-05-19 15:45:15 -0300

    [updated] get user query

--- routes/pg.js
+++ routes/pg.js
@@ -2,19 +2,16 @@
 
 module.exports = async function (fastify, opts) {
   fastify.get("/user", function (request, reply) {
-    fastify.pg.query(
-      'SELECT id, name, email FROM "user"',
-      function onResult(err, result) {
-        reply.send(err || result.rows);
-      }
-    );
+    fastify.pg.query('SELECT * FROM "user"', function onResult(err, result) {
+      reply.send(err || result.rows);
+    });
   });
 
   fastify.post("/user", (request, reply) => {
     fastify.pg.query(
-      `INSERT INTO "user" (id, name, email, password) VALUES('389f158a-ffb1-43c6-9ed6-ee9eae004394', '${request.body.name}', '${request.body.email}', '${request.body.password}') RETURNING *`,
+      `INSERT INTO "user" (id, name, email, password) VALUES('${request.body.id}', '${request.body.name}', '${request.body.email}', '${request.body.password}') RETURNING *`,
       function onResult(err, result) {
-        reply.send(err || result);
+        reply.send(err || result.rows);
       }
     );
   });
