e3f6f5994500bb73d8a89f736839c851d316e41a
Elias Cyrino de Assis <eliascyrino@gmail.com>
2023-05-22 12:38:57 -0300

    [fixed] generating token from email and password

--- routes/root.js
+++ routes/root.js
@@ -2,20 +2,10 @@
 
 module.exports = async function (fastify, opts) {
   fastify.post("/auth", async (request, reply) => {
-    const userId = await fastify.selectUserIdByNameAndPassword(
-      request.body.name,
-      request.body.password
-    );
     const token = fastify.jwt.sign({
-      userId: userId,
+      email: request.body.email,
+      password: request.body.password,
     });
     reply.send({ token });
   });
-  fastify.get(
-    "/protected",
-    { onRequest: [fastify.authenticate] },
-    async function (request, reply) {
-      return request.user;
-    }
-  );
 };
